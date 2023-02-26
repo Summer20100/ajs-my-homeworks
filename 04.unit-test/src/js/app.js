e6f637fb06ba09edee71de2a864f551394f8b773
Dmitrii Bulatov <bulati484@gmail.com>
2023-02-26 12:20:02 +0300

    final corrections

--- 04.unit-test/src/js/app.js
+++ 04.unit-test/src/js/app.js
@@ -6,17 +6,17 @@ console.log('worked');
 console.log(sum([1, 2]));
 
 export function healthLine(hero) {
-    let damage;
-    if (hero.health <= 15) {
-        damage = 'critical';
-    } else if (hero.health < 50) {
-        damage = 'wounded';
-    } else {
-        damage = 'healthy';
-    }
-    return damage;
+  let damage;
+  if (hero.health <= 15) {
+    damage = 'critical';
+  } else if (hero.health < 50) {
+    damage = 'wounded';
+  } else {
+    damage = 'healthy';
+  }
+  return damage;
 }
 
 export function sortHeroes(heroes) {
-    return heroes.sort((a, b) => ((+a.health) > (+b.health) ? -1 : 1));
+  return heroes.sort((a, b) => ((+a.health) > (+b.health) ? -1 : 1));
 }
